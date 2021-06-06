1d2af44b011befc5277ac712b3f8c6a19be29e83
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-06-06 19:34:14 +0200

    Update audio-setup.sh

--- audio-setup.sh
+++ audio-setup.sh
@@ -1,4 +1,6 @@
 #!/data/data/com.termux/files/usr/bin/bash
+apt update
+apt upgrade
 pkg install wget
 wget https://andronixos.sfo2.cdn.digitaloceanspaces.com/OS-Files/setup-audio.sh
 chmod +x setup-audio.sh
