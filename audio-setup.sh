c8c2fd6a3ae9e36014f84d3a1ae4fe198252505a
Andre-cmd-rgb <85292605+Andre-cmd-rgb@users.noreply.github.com>
2021-06-08 09:55:07 +0200

    Delete audio-setup.sh

--- audio-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-apt update
-apt upgrade
-pkg install wget
-wget https://andronixos.sfo2.cdn.digitaloceanspaces.com/OS-Files/setup-audio.sh
-chmod +x setup-audio.sh
-./setup-audio.sh
-pulseaudio --start
